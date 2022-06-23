d755eec02dc756f5b59824bab4caa60730532d2d
Felipe <fvirasoro@gmail.com>
2022-06-23 20:14:26 +0200

    cambios

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -2,13 +2,13 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Home</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Aca vas a ver algunas funcionalidades proximamente</ion-title>
         </ion-toolbar>
       </ion-header>
     
